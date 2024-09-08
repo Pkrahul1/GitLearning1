ca95d08be47d8412cadafe6f97c6be9f5c8ce83c
rahul <pkrahul.ks16@gmail.com>
2024-09-08 13:24:59 +0530

    new git file

---
+++ LearningProjects/GitClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProjects
+{
+    internal class GitClass
+    {
+        public void LearnGit()
+        {
+            // first line added.
+        }
+    }
+}
